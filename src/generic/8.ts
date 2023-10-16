b6887a1478289ebe8b57d917cfbee2bbadf1e32c
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-10-16 16:28:24 +0300

    Наслідування

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -185,22 +185,84 @@
 
 // grit(max);
 
-interface IPerson {
-  name: string;
-  age: number;
+// interface IPerson {
+//   name: string;
+//   age: number;
 
-  greet(phrase: string): void;
-}
+//   greet(phrase: string): void;
+// }
 
-let user: IPerson;
+// let user: IPerson;
 
-user = {
-  name: 'Anthony',
-  age: 21,
+// user = {
+//   name: 'Anthony',
+//   age: 21,
 
-  greet(phrase) {
-    console.log(phrase + '' + this.name, this.age);
-  },
-};
+//   greet(phrase) {
+//     console.log(phrase + '' + this.name, this.age);
+//   },
+// };
 
-user.greet('Вітання всім, я ');
+// user.greet('Вітання всім, я ');
+
+// class Team {
+//   members: Programmer[];
+//   constructor(members: Programmer[]) {
+//     this.members = members;
+//   }
+//   startProject() {
+//     this.members.forEach(member => member.code());
+//   }
+// }
+// class Programmer {
+//   code() {
+//     console.log('Coding...');
+//   }
+// }
+// const programmers = [new Programmer(), new Programmer()];
+// const team = new Team(programmers);
+// team.startProject();
+
+//Агрегацію. Клас містить інший клас, але не керує його життєвим циклом.
+
+// class Computer {
+//   processor: Processor;
+//   constructor() {
+//     this.processor = new Processor();
+//   }
+//   start() {
+//     this.processor.processData();
+//   }
+// }
+// class Processor {
+//   processData() {
+//     console.log('Processing data...');
+//   }
+// }
+// const computer = new Computer();
+// computer.start();
+
+//Правильно! Композицію. Клас містить у собі інший клас та керує його життєвим циклом.
+
+// class Car {
+//   driver: Driver | null = null;
+//   setDriver(driver: Driver) {
+//     this.driver = driver;
+//   }
+//   startJourney() {
+//     if (this.driver) {
+//       this.driver.drive();
+//     }
+//   }
+// }
+// class Driver {
+//   drive() {
+//     console.log('Driving...');
+//   }
+// }
+// const driver = new Driver();
+// const car = new Car();
+// car.setDriver(driver);
+// car.startJourney();
+
+//Асоціація. Класи взаємодіють один з одним.
